e15088a2327fa53be4545bcb2deac79633db643b
SBeltgens <sg.beltgens@student.avans.nl>
2025-03-28 13:20:00 +0100

    Removed all build errorrs

--- APIproject/Program.cs
+++ APIproject/Program.cs
@@ -11,24 +11,23 @@ var DbConnectionString = "Server=tcp:avansict2229187.database.windows.net,1433;I
 // Add services to the container.
 builder.Services.AddAuthorization();
 builder.Services
-    .AddIdentityApiEndpoints<IdentityUser>()
-    .AddDapperStores(options => { options.ConnectionString = DbConnectionString; });
-builder.Services
     .AddIdentityApiEndpoints<IdentityUser>(options =>
-     {
-         options.User.RequireUniqueEmail = true;
-         options.SignIn.RequireConfirmedPhoneNumber = true;
+    {
+        options.User.RequireUniqueEmail = true;
+        options.SignIn.RequireConfirmedPhoneNumber = true;
 
-         options.Password.RequireDigit = false;
-         options.Password.RequireLowercase = true;
-         options.Password.RequireUppercase = true;
-     });
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+    })
+    .AddDapperStores(options => { options.ConnectionString = DbConnectionString; });
+
 builder.Services
     .AddOptions<BearerTokenOptions>(IdentityConstants.BearerScheme)
-    .Configure(options => {
-        options.BearerTokenExpiration = TimeSpan.FromMinutes(value: 60);
+    .Configure(options =>
+    {
+        options.BearerTokenExpiration = TimeSpan.FromMinutes(60);
     });
-
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -36,24 +35,25 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 app.UseAuthorization();
-app.MapGroup(prefix: "/account")
-.MapIdentityApi<IdentityUser>();
-     app.MapPost(pattern: "/account/logout",
-         async (SignInManager<IdentityUser> signInManager,
-         [FromBody] object empty) => {
-             if (empty != null)
-             {
-                 await signInManager.SignOutAsync();
-                 return Results.Ok();
-             }
-             return Results.Unauthorized();
-         })
-         .RequireAuthorization();
-app.MapControllers()
-   .RequireAuthorization();
 
-     // Configure the HTTP request pipeline.
-     if (app.Environment.IsDevelopment())
+app.MapGroup("/account").MapIdentityApi<IdentityUser>();
+
+app.MapPost("/account/logout",
+    async (SignInManager<IdentityUser> signInManager, [FromBody] object empty) =>
+    {
+        if (empty != null)
+        {
+            await signInManager.SignOutAsync();
+            return Results.Ok();
+        }
+        return Results.Unauthorized();
+    })
+    .RequireAuthorization();
+
+app.MapControllers().RequireAuthorization();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
