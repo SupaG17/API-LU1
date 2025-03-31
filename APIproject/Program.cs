f5475e258929b4e990b33bade8faf1a0cd4854a0
SupaG17 <supag17@outlook.com>
2025-03-31 13:56:07 +0200

    UserSecrets added + connectionstring

--- APIproject/Program.cs
+++ APIproject/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+if (string.IsNullOrWhiteSpace(sqlConnectionString))
+    throw new InvalidProgramException("Configuration variable SqlConnectionString not found");
+
 var DbConnectionString = "Server=tcp:avansict2229187.database.windows.net,1433;Initial Catalog=db2229187;Persist Security Info=False;User ID=SupaGmin;Password={Ado-170306};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
 
 // Add services to the container.
